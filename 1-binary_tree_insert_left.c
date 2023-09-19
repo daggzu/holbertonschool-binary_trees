e011641b6841e09910487569b9aa0156e67c8824
daggzu <112585566+daggzu@users.noreply.github.com>
2023-09-19 18:49:30 -0400

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -6,12 +6,12 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
     if (parent == NULL)
         return NULL;
 
-    // Allocate memory for the new node
+    
     binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
     if (new_node == NULL)
         return NULL;
 
-    // Initialize the new node
+    
     new_node->n = value;
     new_node->parent = parent;
     new_node->left = NULL;
