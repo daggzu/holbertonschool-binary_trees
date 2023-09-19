1e9cb027213a6239d23a800eeca366f00b3c0d3f
daggzu <112585566+daggzu@users.noreply.github.com>
2023-09-19 18:47:17 -0400

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -18,7 +18,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
         return NULL;
     }
 
-    // Initialize the new node
+
     new_node->n = value;
     new_node->parent = parent;
     new_node->left = NULL;
