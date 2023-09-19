31539656f81196c7ffd33013664321261abffa2e
daggzu <diegog8603@gmail.com>
2023-09-19 19:16:58 -0400

    uodate

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - delete whole binary tree.
+ * @tree: pointer to binary tree.
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+
+if (tree == NULL)
+{
+return;
+}
+
+	binary_tree_delete(tree->left); 
+	binary_tree_delete(tree->right); 
+
+	free(tree); /*free mermory of binary tree.*/
+}
