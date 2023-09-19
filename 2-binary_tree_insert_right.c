407ae51a300ef8c34a0794c567e053f3a8cce9c2
daggzu <diegog8603@gmail.com>
2023-09-19 19:12:56 -0400

    yooo

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,16 +11,22 @@
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
     if (parent == NULL || value == '\0')
-        return NULL;
+    {
+        return NULL; 
+    }
+   
+   *new_node = malloc(sizeof(binary_tree_t));
 
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
     if (new_node == NULL)
-        return NULL;
+       {
+            return NULL;
+       }
 
     new_node->n = value;
     new_node->parent = parent;
     new_node->left = NULL;
-
+    new_node->right = NULL;
+    
     if (parent->right != NULL)
     {
         new_node->right = parent->right;
