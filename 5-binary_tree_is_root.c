88dfb2f0366b8496d31eb257251acd04ab960765
daggzu <112585566+daggzu@users.noreply.github.com>
2023-09-19 19:58:42 -0400

    idk

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - checks if a node is a leaf.
+ * @node: pointer to the binary tree.
+ *
+ * Return: Found root.
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+	{
+		return (0);
+	}
+
+	if (node->parent == NULL)
+	{
+		return (1);
+	}
+
+	return (0);
+}
