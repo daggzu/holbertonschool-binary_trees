4811ead875bb9eaed330863d1bef920dc80d6b98
daggzu <112585566+daggzu@users.noreply.github.com>
2023-09-20 16:44:50 -0400

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,29 +1,26 @@
 #include "binary_trees.h"
-#include <stdlib.h>
+
 /**
- 
-binary_tree_node - Create new node in binary tree.
-@parent: Origin node for creation.
-@value: Data the node holds.
-*
-Return: New node.
-*/
+ * binary_tree_node - Create new node in binary tree.
+ * @parent: Origin node for creation.
+ * @value: Data the node holds.
+ *
+ * Return: New node.
+ */
 
-binary_tree_t binary_tree_node(binary_tree_tparent, int value)
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    // Allocate memory for the new node
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t)); /*Allocate memory.*/
 
-    if (new_node == NULL) {
-        // Failed to allocate memory
-        return NULL;
-    }
+	if (new_node == NULL) /*Error Check.*/
+	{
+		return (NULL);
+	}
 
+	new_node->n = value; /*Content the node holds.*/
+	new_node->parent = parent; /*Birthplace.*/
+	new_node->left = NULL; /*No sibling node*/
+	new_node->right = NULL; /*No sibling node.*/
 
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
-
-    return new_node;
+	return (new_node);
 }
