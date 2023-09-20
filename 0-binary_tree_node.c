305c1460e9553d1239b8a46c38cba2d28a004362
daggzu <112585566+daggzu@users.noreply.github.com>
2023-09-20 16:30:35 -0400

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,11 +10,11 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    // Allocate memory for the new node
+   
     binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
     if (new_node == NULL) {
-        // Failed to allocate memory
+        
         return NULL;
     }
 
