43d20f9a2052af1aedad82abfe658f3a67b377d0
daggzu <112585566+daggzu@users.noreply.github.com>
2023-09-20 16:46:31 -0400

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,26 +1,41 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - Create new node in binary tree.
- * @parent: Origin node for creation.
+ * binary_tree_insert_right - insert node as the right child.
+ * @parent: Origin node of creation.
  * @value: Data the node holds.
  *
- * Return: New node.
+ * Return: right inserted node.
  */
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t)); /*Allocate memory.*/
+	binary_tree_t *new_node;
+
+	if (parent == NULL || value == '\0') /*Error Check.*/
+	{
+		return (NULL);
+	}
+
+	new_node = malloc(sizeof(binary_tree_t));
 
 	if (new_node == NULL) /*Error Check.*/
 	{
 		return (NULL);
 	}
 
-	new_node->n = value; /*Content the node holds.*/
-	new_node->parent = parent; /*Birthplace.*/
-	new_node->left = NULL; /*No sibling node*/
-	new_node->right = NULL; /*No sibling node.*/
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	if (parent->right != NULL) /*Insertion to the left.*/
+	{
+		new_node->right = parent->right;
+		new_node->right->parent = new_node;
+	}
+
+	parent->right = new_node;
 
 	return (new_node);
 }
